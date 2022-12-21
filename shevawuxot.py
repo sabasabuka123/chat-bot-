49098fb1dd877002b20775c9db7ce600e15cfbc6
sabasabuka123 <119455582+sabasabuka123@users.noreply.github.com>
2022-12-21 22:45:28 +0400

    Add files via upload

---
+++ shevawuxot.py
@@ -0,0 +1,14 @@
+import random
+import pyautogui as pg
+import time
+
+# animal=("monkey",'dog',"pig",'cow','snake')
+
+time.sleep(8)
+
+for i in range(100): 
+
+    # a=random.choice(animal)
+    pg.write('https://www.youtube.com/watch?v=-JGWm1tg4mk')
+    pg.press('enter')
+    
